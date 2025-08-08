bf679f61dc9c3b805a8167aa032a7d5c9e6ee92c
Yael Kroy <yaelkroy@gmail.com>
2025-08-08 21:21:51 +0300

    modified GPL header for telegra,_handlers.py

--- enkibot/core/telegram_handlers.py
+++ enkibot/core/telegram_handlers.py
@@ -1,7 +1,19 @@
 ﻿# enkibot/core/telegram_handlers.py
 # EnkiBot: Advanced Multilingual Telegram AI Assistant
 # Copyright (C) 2025 Yael Demedetskaya <yaelkroy@gmail.com>
-# (Your GPLv3 Header)
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program. If not, see <https://www.gnu.org/licenses/>.
 
 import logging
 import asyncio
